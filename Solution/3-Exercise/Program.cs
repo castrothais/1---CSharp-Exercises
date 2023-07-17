c94baef619536b9bc39a4932a0c98ac2b965ee2c
Thais Sampaio <thais_castrocs@outlook.com>
2023-07-17 00:27:24 -0300

    feat: count characters

---
+++ Solution/3-Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise3
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Digite um texto: ");
+            string text = Console.ReadLine();
+            Console.WriteLine($"O seu texto possui {text.Length} caracteres.");
+        }
+    }
+}
