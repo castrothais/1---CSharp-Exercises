6c9683ab3ab8f58cd45f7cefa77d8a03c9446cb9
Thais Sampaio <thais_castrocs@outlook.com>
2023-07-17 00:28:16 -0300

    feat: find circle area

---
+++ Solution/4-Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise3
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Digite o valor do Raio: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area;
+            if (radius >= 0 )
+            {
+                area = (Math.Pow(radius,2) * Math.PI);
+                Console.WriteLine($"O raio do círculo é de: {area.ToString("0.00")}");
+            } 
+            else
+            {
+                Console.WriteLine($"Raio inválido! Por favor, use número positivo.");
+            }
+        }
+    }
+}
