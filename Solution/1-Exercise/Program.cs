7761dedcdd1b88ab6a54ebf89c37657a579d80a9
Thais Sampaio <thais_castrocs@outlook.com>
2023-07-17 00:25:21 -0300

    feat: print name

---
+++ Solution/1-Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise1
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Me informe o seu nome:");
+            string nameUser = Console.ReadLine();
+
+            if(InputIsValidText(nameUser) && nameUser != string.Empty)
+            {
+                Console.WriteLine($"Olá, {nameUser}! Seja bem-vindo ao meu repositório do Coding Tank de C#");
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida! Por favor, preencher apenas com texto.");
+            }
+
+        }
+
+        static bool InputIsValidText(string nameUser)
+        {
+            foreach (char c in nameUser)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
