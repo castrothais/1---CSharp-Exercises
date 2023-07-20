df9bb6d13d7d09295c89edab38a7b27e4da71ea2
Thais Sampaio <thais_castrocs@outlook.com>
2023-07-20 16:47:26 -0300

    fix: exercise 4 - treatment without exception

--- Solution/4-Exercise/Program.cs
+++ Solution/4-Exercise/Program.cs
@@ -7,16 +7,17 @@ namespace Exercise3
         public static void Main(string[] args)
         {
             Console.WriteLine("Olá! Digite o valor do Raio: ");
-            double radius = double.Parse(Console.ReadLine());
-            double area;
-            if (radius >= 0 )
+            string radiusInput = Console.ReadLine();
+
+            double radius;
+            if (double.TryParse(radiusInput, out radius) && radius >= 0)
             {
-                area = (Math.Pow(radius,2) * Math.PI);
+                double area = (Math.Pow(radius, 2) * Math.PI);
                 Console.WriteLine($"O raio do círculo é de: {area.ToString("0.00")}");
-            } 
+            }
             else
             {
-                Console.WriteLine($"Raio inválido! Por favor, use número positivo.");
+                Console.WriteLine($"Raio inválido! Por favor, use apenas número e que seja positivo.");
             }
         }
     }
